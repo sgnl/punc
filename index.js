6886b88ad7ad15520e1d99f56d488045b0c39a60
sgnl <rayrfarias@gmail.com>
2016-02-24 11:34:34 -1000

    adds more mock code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Fs = require('fs');
+
 function PuncModule (opts) {
   let _filePath, module = {};
  
@@ -7,6 +9,10 @@ function PuncModule (opts) {
     _filePath = filePath;
 
     return 'file path accepted';
+  };
+
+  module.parse = () => {
+    return 'parsing file';
   };
 
   return module;
