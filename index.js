7818250f6fd6f6257ae2343fafa941274f88f374
sgnl <rayrfarias@gmail.com>
2016-02-24 11:26:10 -1000

    WIP: adds mock code

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-exports.punc = function() {
-    console.log('I am Punc.')
-};
+'use strict';
+
+function PuncModule (opts) {
+  let _filePath, module = {};
+ 
+  module.load = (filePath) => {
+    _filePath = filePath;
+
+    return 'file path accepted';
+  };
+
+  return module;
+}
+
+module.exports = PuncModule;
+
