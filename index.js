8367abbd35ba2a7d1ab65c48e9e3e416ee11d3bc
sgnl <rayrfarias@gmail.com>
2016-02-26 15:04:59 -1000

    resolves #1 - parse plain text files
    able to read utf8 encoded files

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
 'use strict'
 
-const stp = require('stream-to-promise')
-const readFile = require('fs').createReadStream;
+const Promise = require('bluebird')
+const Through2 = require('through2')
+const ReadFile = require('fs').createReadStream
+const Reduce = Array.prototype.reduce;
 
 /* MODULE */
-function PuncModule (opts) {
-  let module = {}
+function Punc(filePath, options){
+  if (!options) {
+    options = { encoding: 'utf8' }
+  } else if (typeof options === 'string') {
+    options = { encoding: options }
+  } else {
+    throw new TypeError('Punc: expected options to be either an object or a string, ' +
+    'but got ' + typeof options + ' instead');
+  }
 
-  return (filePath) => {
-    return new Promise((resolve, reject) => {
-      let promise =
-        stp(readFile(filePath))
-          .then(resolve)
-          .catch(reject)
+  let punctuationsSeen = { ';': 0
+    , ':': 0
+    , "'": 0
+    , '"': 0
+    , ',': 0
+    , '!': 0
+    , '?': 0
+    , '.': 0
+    , '(': 0
+    , ')': 0
+    , '-': 0
+    }
 
-      return promise
-    })
-  };
+  let punctuationsOnly = [];
+
+  console.log('inside Punc Module')
+
+  return new Promise((resolve, reject) => {
+    ReadFile(filePath, options.encoding)
+      /**
+       * remove words, leave only punctuations and count them too!
+       * ; : ' " , ! ? . ( ) -
+       */
+      .pipe(Through2.obj(function(chunk, _, callback) {
+
+        for (let i = 0; i < chunk.length; i++) {
+
+          if ( chunk[i] in punctuationsSeen ) {
+            punctuationsSeen[ chunk[i] ]++
+            punctuationsOnly.push(chunk[i])
+          }
+        }
+        return callback(null, chunk)
+      }))
+      .on('data', data => {
+
+      })
+      .on('end', _ => {
+        resolve({
+          body: punctuationsOnly.join(''),
+          count: punctuationsSeen
+        })
+      })
+      .on('error', error => reject(error))
+  })
 }
 
 /* THE TRUTH IS OUT THERE */
-module.exports = PuncModule
-
-/* HELPERS */
-function countPunctuation (buffer) {
-  buffer.on('data')
-}
+module.exports = Punc
